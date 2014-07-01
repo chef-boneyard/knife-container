2b4d4eb5182685ff7e6bad3f3734f47d36822e66
Tom Duffield <tom@getchef.com>
2014-07-01 15:48:20 -0500

    explicitly enabled the should syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,6 +26,9 @@ end
 RSpec.configure do |c|
   c.include TestHelpers
 
+  c.expect_with :rspec do |config|
+    config.syntax = [:should, :expect]
+  end
   c.filter_run :focus => true
   c.run_all_when_everything_filtered = true
   c.treat_symbols_as_metadata_keys_with_true_values = true
